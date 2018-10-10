4811c3c1a66df3f044fe633fd22cad7651b24285
Edson Ariel Aju Garcia <avataredson@gmail.com>
2018-10-10 13:43:40 -0600

    Version 1.2.2

--- ORMOlimpEsp/build/generated-sources/ap-source-output/Entities/Atleta_.java
+++ ORMOlimpEsp/build/generated-sources/ap-source-output/Entities/Atleta_.java
@@ -13,7 +13,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-08T22:35:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-09T22:00:52")
 @StaticMetamodel(Atleta.class)
 public class Atleta_ { 
 
--- ORMOlimpEsp/build/generated-sources/ap-source-output/Entities/Estado_.java
+++ ORMOlimpEsp/build/generated-sources/ap-source-output/Entities/Estado_.java
@@ -16,7 +16,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-05T14:25:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-09T22:00:52")
 @StaticMetamodel(Estado.class)
 public class Estado_ { 
 
